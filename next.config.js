746fa561f57fbf9344d6294360e7ae0b5e9c07bc
David Lanigan <dav.lanigan@gmail.com>
2022-11-18 07:00:58 -0500

    upd

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: "."
+  assetPrefix: "https://dave-lanigan.github.io"
 }
 
 module.exports = nextConfig
